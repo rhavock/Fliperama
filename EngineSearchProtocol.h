33df18f3bdd432ae684a23edd41b5f179dc41ec5
Rodrigo Heleno <rhavock@MacBook-Pro-de-rodrigo.local>
2017-06-12 22:33:01 -0300

    Engine search

--- EngineSearchProtocol.h
+++ EngineSearchProtocol.h
@@ -1,13 +1,6 @@
-//
-//  EngineSearchProtocol.h
-//  IGDBBR
-//
-//  Created by Rodrigo Heleno on 12/06/17.
-//  Copyright © 2017 RodrigoHeleno. All rights reserved.
-//
 
-#ifndef EngineSearchProtocol_h
-#define EngineSearchProtocol_h
+@protocol EngineSearchProtocol
 
 
-#endif /* EngineSearchProtocol_h */
+
+@end
